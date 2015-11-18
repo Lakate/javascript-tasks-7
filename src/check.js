b89ad8870ccca957c3bbd3dfb2261183fcc85c48
Kate Latypova <katelat3009@gmail.com>
2015-11-18 19:16:58 +0500

    bonus

--- src/check.js
+++ src/check.js
@@ -12,17 +12,57 @@ METHODS_FOR.array = METHODS_FOR.object.concat(['hasLength']);
 exports.init = function () {
     Object.defineProperty(Object.prototype, 'check', {
         get: function () {
-            /** Получаем вспомогательные методы */
-            var helperMethods = getHelperMethods();
-            /** Получаем тип объекта */
-            var typeOfObject = getType.call(this);
-            METHODS_FOR[typeOfObject].forEach(function (method) {
-                helperMethods[method] = helperMethods[method].bind(this);
-            }, this);
-            return helperMethods;
+            /** Добавляем .not */
+            Object.defineProperty(Object.prototype, 'not', {
+                get: function () {
+                    return extendBasePrototype.call(this, false);
+                }.bind(this),
+                configurable: true
+            });
+            return extendBasePrototype.call(this, true);
         },
         configurable: true
     });
+};
+
+/**
+ * Возвращает методы, которыми будем расширять прототип.
+ * Зависит от typeOfProperty: если он true, то вызвались из check, если false - из not
+ * @param {boolean} typeOfProperty
+ */
+function extendBasePrototype(typeOfProperty) {
+    /** Получаем вспомогательные методы */
+    var helperMethods = getHelperMethods.call(this);
+    /** Получаем тип объекта */
+    var typeOfObject = getType.call(this);
+    METHODS_FOR[typeOfObject].forEach(function (method) {
+        helperMethods[method] = helperMethods[method].bind(this, typeOfProperty);
+    }, this);
+    return helperMethods;
+};
+
+/**
+ * Обертка для объектов
+ */
+exports.wrap = function (obj) {
+    if (obj === null) {
+        return {
+            get: function () {
+                return null;
+            },
+            isNull: function () {
+                return true;
+            }
+        };
+    }
+
+    Object.defineProperty(Object.prototype, 'isNull', {
+        get: function () {
+            return false;
+        }
+    });
+    exports.init.call(this);
+    return obj;
 };
 
 /**
@@ -30,51 +70,59 @@ exports.init = function () {
  */
 function getHelperMethods() {
     return {
-        containsKeys: function (keys) {
+        containsKeys: function () {
+            var args = [].slice.call(arguments, -2);
+            var typeOfProperty = args[0];
+            var keys = args[1];
             var objectKeys = Object.keys(this);
-            return keys.every(function (key) {
+            var answer = keys.every(function (key) {
                 return objectKeys.indexOf(key) !== -1;
             });
+            return getAnswer(typeOfProperty, answer);
         },
 
-        hasKeys: function (keys) {
-            return Object.keys(this).length === keys.length && this.check.containsKeys(keys);
+        hasKeys: function (typeOfProperty, keys) {
+            return getAnsToHasMethods.call(this, typeOfProperty, keys, 'Keys');
         },
 
-        containsValues: function (keys) {
+        containsValues: function () {
+            var args = [].slice.call(arguments, -2);
+            var typeOfProperty = args[0];
+            var values = args[1];
             var cb = function (key) {
                 return this[key];
             };
             var objectValues = Object.keys(this).map(cb, this);
-            return keys.every(function (key) {
+            var answer = values.every(function (key) {
                 return objectValues.indexOf(key) !== -1;
             });
+            return getAnswer(typeOfProperty, answer);
         },
 
-        hasValues: function (keys) {
-            return Object.keys(this).length === keys.length && this.check.containsValues(keys);
+        hasValues: function (typeOfProperty, keys) {
+            return getAnsToHasMethods.call(this, typeOfProperty, keys, 'Values');
         },
 
-        hasValueType: function (key, type) {
+        hasValueType: function (typeOfProperty, key, type) {
             if ([String, Number, Function, Array].indexOf(type) === -1) {
                 return 'ERROR! Wrong type!';
             }
             if (!this.hasOwnProperty(key)) {
                 return 'ERROR! Wrong field!';
             }
-            return this[key] === type(this[key]);
+            return getAnswer(typeOfProperty, areEqual(this[key], type(this[key])));
         },
 
-        hasLength: function (length) {
-            return this.length === length;
+        hasLength: function (typeOfProperty, length) {
+            return getAnswer(typeOfProperty, areEqual(this.length, length));
         },
 
-        hasParamsCount: function (length) {
-            return this.length === length;
+        hasParamsCount: function (typeOfProperty, length) {
+            return this.check.hasLength.call(this, typeOfProperty, length);
         },
 
-        hasWordsCount: function (count) {
-            return this.split(' ').length === count;
+        hasWordsCount: function (typeOfProperty, count) {
+            return getAnswer(typeOfProperty, areEqual(this.split(' ').length, count));
         }
     };
 }
@@ -93,4 +141,21 @@ function getType() {
         case String.prototype: return 'string';
     }
     return 'object';
+}
+
+function areEqual(a, b) {
+    return a === b;
+}
+
+/** Сначала считаем всегда для check, а потом инвертируем ответ, если нужно */
+function getAnsToHasMethods(typeOfProperty, keys, param) {
+    return getAnswer(typeOfProperty, areEqual(Object.keys(this).length, keys.length) &&
+        this.check['contains' + param](true, keys));
+}
+
+function getAnswer(typeOfProperty, answer) {
+    if (typeOfProperty) {
+        return answer;
+    }
+    return !answer;
 }
